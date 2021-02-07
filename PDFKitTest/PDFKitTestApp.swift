ae71867171ac3495170ad65279a68e6c57d16d82
Bart van Kuik <bart@dutchvirtual.nl>
2021-02-07 15:34:22 +0100

    Initial Commit

---
+++ PDFKitTest/PDFKitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDFKitTestApp.swift
+//  PDFKitTest
+//
+//  Created by Bart van Kuik on 07/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct PDFKitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
